4a8ace7908064ff8a5a3df5b74f8ad2b1a709fbb
ruchitdonga <59388176+ruchitdonga@users.noreply.github.com>
2021-02-03 14:17:06 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.Token = "NzM2NTUyNjg2MTY5NjIwNDkw.XxweJA.2ditcN3pJkkg3v8Nb305V_5FLV8"; //Bot Token - Important
+exports.Owner = "486152414601412608"; //Bot Owner ID - Important
+exports.Default_Prefix = "r!"; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
+exports.Support = `https://discord.gg/6qCfQSVW24`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
