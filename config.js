5262c9addfd0f869954b87724e92cafea0f4b0a4
ruchitdonga <59388176+ruchitdonga@users.noreply.github.com>
2021-02-03 14:18:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.Token = "NzM2NTUyNjg2MTY5NjIwNDkw.XxweJA.2ditcN3pJkkg3v8Nb305V_5FLV8"; //Bot Token - Important
+exports.Token = ""; //Bot Token - Important
 exports.Owner = "486152414601412608"; //Bot Owner ID - Important
 exports.Default_Prefix = "r!"; //Bot Default Prefix (Examples: A!, !)- Important
 exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
